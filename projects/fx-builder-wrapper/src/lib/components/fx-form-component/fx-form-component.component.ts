7fda6bbdb94243258604a99e37a64055cdc86d31
Neeraj Gupta <neeraj.gupta@instantsys.com>
2026-05-13 19:06:18 +0530

    remove debug console log for normalized variables

--- projects/fx-builder-wrapper/src/lib/components/fx-form-component/fx-form-component.component.ts
+++ projects/fx-builder-wrapper/src/lib/components/fx-form-component/fx-form-component.component.ts
@@ -72,7 +72,6 @@ export class FxFormWrapperComponent implements OnChanges, OnInit {
         result[key] = val;
       }
     }
-    // console.log('Normalized variables:', result);
     return result;
   }
 
