cb1d82486bde09aa6b733eeaad729f002f18818e
Swaraj Kumar <swaraj.kumar@instantsys.com>
2026-08-03 13:51:00 +0530

    fix: update title assignment logic in uploader component

--- projects/fx-builder-wrapper/src/lib/components/uploader/uploader.component.ts
+++ projects/fx-builder-wrapper/src/lib/components/uploader/uploader.component.ts
@@ -556,7 +556,7 @@ ngAfterViewInit(): void {
             },
             result:      item.fileUrl,       // pre-signed URL for display
             name:        fileName,
-            title:       (item.title || fileName || '').substring(0, 26),
+            title:       item.title || fileName || '',
             notes:       item.notes || '',
             categoryId:  item.categoryId || '',
             // isAttached:  true,
