3c225bd3e2afeef946f1af6f7e97c069ed3707ee
Neeraj Gupta <neeraj.gupta@instantsys.com>
2026-08-12 13:11:14 +0530

    set default PDF URL in uploader component and log file URL on load

--- projects/fx-builder-wrapper/src/lib/components/uploader/uploader.component.ts
+++ projects/fx-builder-wrapper/src/lib/components/uploader/uploader.component.ts
@@ -45,7 +45,7 @@ export class UploaderComponent extends FxBaseComponent implements OnInit, AfterV
   fileVisible: boolean = false;
   selecteImageUrl: string = '';
   fileUrl: SafeResourceUrl | null = null;
-  pdfUrl: string = '';
+  pdfUrl: string = 'https://clove-qa.s3.ap-southeast-1.amazonaws.com/FileModule/Smile_Simulation_Report_Bigb_e4407e96-399e-439e-acdb-ea72b8dd794f.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20260812T073821Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=AKIAZUW65RQXTX5HWE6X%2F20260812%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Signature=f85b559cde3aa23c17b754d8007d6ce65d20f64b34a74b7423d65322687a4a72';
   fileType: string | null = null;
   fileName: string | null = null;
   private destroy$ = new Subject<Boolean>();
@@ -1045,6 +1045,7 @@ ngAfterViewInit(): void {
 
 
     this.loadFile(localUrl, name);
+    console.log('File URL:', localUrl);
     this.fileVisible = true;
   }
 
@@ -1089,7 +1090,7 @@ ngAfterViewInit(): void {
 
     if (extension === 'pdf') {
       // Native PDF rendering
-      this.pdfUrl = url;
+      // this.pdfUrl = url;
     }
     else if (['doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'csv', '.stl'].includes(extension)) {
       // Microsoft Office Viewer for Office files
